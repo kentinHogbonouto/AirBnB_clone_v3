0c615268308a530d334f6be140413f7ca5a581c1
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-11-19 01:22:10 +0100

    4. Some stats task

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,11 +2,19 @@
 """This module is a blueprint"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status")
 def status():
-    status = {
-            "status": "OK"
-            }
-    return status
+        return jsonify({'status': 'OK'})
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+        return jsonify({
+                "amenities": storage.count("Amenity"),
+                "places": storage.count("Place"),
+                "reviews": storage.count("Review"),
+                "states": storage.count("state"),
+                "users": storage.count("User")
+        })
